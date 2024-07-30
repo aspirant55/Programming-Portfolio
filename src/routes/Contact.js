3d27c124288def4148cab10ae177fc36034ae793
Orion Joyner <your-email@example.com>
2024-07-29 23:13:37 -0400

    first commit

---
+++ src/routes/Contact.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import HeroImg3 from "../components/HeroImg3";
+
+const Contact = () => {
+  return (
+    <div><Navbar />
+    <HeroImg3 heading="CONTACT." text="Looking to chat" />
+    <Footer />
+    </div>
+  );
+}
+
+export default Contact
